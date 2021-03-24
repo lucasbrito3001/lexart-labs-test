1665814c967dc7f15a58df5aa9ca71c1507277a9
Lucas de Brito <devlucasmail@gmail.com>
2021-03-24 12:37:23 -0300

    [WIP] Primeiro Commit - Form
    1 - Adicionado o Formulário
    2 - Implementado o método POST para adicionar um item ao estoque.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,132 @@
+
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <h1>Teste Front-end Lexart Labs</h1>
+    </header>
+
+    <section id="forms">
+      <Form :props-forms="formValues" :actionForm="actionForm" @post-product="postProduct"/>
+    </section>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Form from './components/Form'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Form,
+  },
+  
+  data () {
+    return {
+      formValues: {
+        product: {
+          name: '',
+          brand: ''
+        },
+        client: {
+          name: '',
+          phone: ''
+        },
+        price: '',
+        quantity:'',
+        active: ''
+      },
+
+      actionForm: 'Add Product',
+
+      baseURL: 'https://crudcrud.com/api/e60e84838b744062ae9e9c869beb38d8', // 8
+
+      arrayTableBody: []
+    }
+  },
+
+  mounted() {
+    this.getProduct()
+  },
+
+  methods: {
+    async getProduct() {
+      const req = new Request (
+        `${this.baseURL}/stock`,
+        {
+          method: 'GET',
+          mode: 'cors'
+        }
+      )
+
+      const res = await fetch(req)
+      const { data } = await res.json()
+
+      this.arrayTableBody = data
+
+      console.log(this.arrayTableBody)
+    },
+    async postProduct(formInfos) {
+      const headers = new Headers ()
+
+      headers.append(
+        'Content-type',
+        'application/json;charset=utf-8'
+      )
+
+      const req = new Request (
+        `${this.baseURL}/stock`,
+        {
+          method: 'POST',
+          body: JSON.stringify(formInfos),
+          headers,
+          mode: 'cors',
+          cache: 'default'
+        }
+      )
+
+      const res = await fetch(req)
+      const data = await res.json()
+
+      console.log(data)
+    }
+
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  :root {
+    --default-orange: rgb(231,150,0)
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    color: #2c3e50;
+  }
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 6vh;
+    font-size: .8rem;
+    widows: 100%;
+    background-color: var(--default-orange);
+    color: white;
+  }
+
+  @media(max-width:768px) {
+    html {
+      font-size: 14px;
+    }
+  }
+
 </style>
---
+++ src/components/Form.vue
@@ -0,0 +1,116 @@
+<template>
+    <form @submit.prevent="sendFormAction">
+        <div id="inputs">
+            <fieldset>
+                <legend>Product</legend>
+                <label for="product-name">Name :</label> <br>
+                <input type="text" id="product-name" v-model="propsForms.product.name"> <br>
+                <label for="product-brand">Brand :</label> <br>
+                <input type="text" id="product-brand" v-model="propsForms.product.brand"> <br>
+            </fieldset>
+            
+            <fieldset>
+                <legend>Client</legend>
+                <label for="client-name">Name :</label> <br>
+                <input type="text" id="client-name" v-model="propsForms.client.name"> <br>
+                <label for="client-phone">Phone :</label> <br>
+                <input type="text" id="client-phone" v-model="propsForms.client.phone">
+            </fieldset>
+            <fieldset>
+                <legend>Infos</legend>
+                <label for="price">Price :</label> <br>
+                <input type="number" id="price" v-model="propsForms.price"> <br>
+                <label for="quantity">Quantity :</label> <br>
+                <input type="number" id="quantity" v-model="propsForms.quantity"> <br>
+                <label for="active">Active :</label> <br>
+                <input type="text" id="active" v-model="propsForms.active">
+            </fieldset>
+        </div>
+        <div id="submit">
+            <input type="submit" :value="actionForm" id="submitButton">
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+    props : {
+        propsForms: {
+            type: Object,
+        },
+        actionForm: {
+            type: String
+        }
+    },
+
+    methods: {
+        sendFormAction() {
+            if (this.actionForm == 'Add Product') {
+                this.$emit('post-product', this.propsForms)
+            } else if (this.actionForm == 'Update Product') {
+                this.$emit('put-product', this.propsForms)
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+    form {
+        display: flex;
+        flex-direction: column;
+        margin-top: 2vh;
+    }
+
+    #inputs {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        width: 100%;
+    }
+
+    #submit {
+        margin: 10px auto;
+    }
+
+    #submitButton {
+        width: fit-content;
+        background: var(--default-orange);
+        color: white;
+        border: 1px solid var(--default-orange);
+    }
+
+    #submitButton:hover {
+        background: white;
+        color: var(--default-orange);
+    }
+
+    fieldset {
+        padding: 5px;
+        width: 30%;
+        border-radius: 3px;
+        border: 1px solid var(--default-orange);
+        color: var(--default-orange);
+    }
+
+    input {
+        border: 1px solid #ddd;
+        border-radius: 3px;
+        font-size: 1rem;
+        padding: 2px 5px;
+        width: 100%;
+    }
+
+    @media(max-width: 768px) {
+        #inputs {
+            flex-direction: column;
+        }
+
+        fieldset {
+            width: 95%;
+            margin: auto;
+        }
+    }
+
+</style>
